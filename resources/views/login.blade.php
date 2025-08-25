c20172e705a0f13ac34dc0d9eb0dacaa0180a0bd
KauCavalcante86 <kauad.cavalcante@gmail.com>
2025-08-25 17:04:47 -0300

    feat: adicionar tela de login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ url('css/login.css') }}" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <title>Login</title>
+</head>
+<body>
+
+    <div class="app" id="appImage">
+        <h1 class="slogan">O MELHOR LUGAR PARA UM CINEMINHA</h1>
+    </div>
+
+    <div class="app" id="form">
+
+        <form action="">
+            
+            <h1 class="titulo">Bem vindo de volta!</h1>
+
+            <input type="text" placeholder="Nome de usuario" class="inEmail">
+
+            <input type="email" placeholder="Email" class="inEmail">
+
+            <input type="password" placeholder="Senha" class="inSenha">
+
+            <button class="btn">Entrar</button>
+
+            <p class="txtCadas">Ainda não tem login? <a href="" class="Cadas">Cadastre-se aqui</a></p>
+        </form>
+    </div>
+    
+</body>
+</html>
