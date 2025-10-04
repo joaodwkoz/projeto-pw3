687cc96cc8eae1c7bd8cecc42b03b36172fef2bc
joaoinsatisfatorio <fjoaopedro1302@gmail.com>
2025-10-04 15:03:34 -0300

    fix: login corrigido

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,7 +36,7 @@ class LoginController extends Controller
     {
         return [
             'email' => $request->email,
-            'senha' => $request->password
+            'password' => $request->password
         ];
     }
 }
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\Usuario; // Mude de 'User' para 'Usuario'
+use App\Models\Usuario;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
